bd9669ee85cc454538509ef14521c09e7f96d6da
michaelvanderlyn <mvanderlyn27@gmail.com>
2023-06-08 23:36:34 -0400

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 dotenv.config();
 
 const app: Express = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
@@ -11,6 +17,7 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/ping', (req: Request, res: Response) => {
+    console.log('request',req.url);
     res.send({message: "Pong"});
 });
 
